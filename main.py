24267c6f82bc759c32ac5659acf6ed8d71fb13c3
Your Name <you@example.com>
2024-03-20 10:15:02 +0200

    expensive

--- main.py
+++ main.py
@@ -15,18 +15,39 @@
 # - iespēja apskatīt vidējo izdevumu summu
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
+import json
 
-expenses = []
-
+expenses_file = open ('expenses.json')
+expenses = json.load(expenses_file)
+expenses_file.close()
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
-pass
+expenses_comp=expenses.copy()
+def sort_price(expenses):
+    return int(expenses["sum"])
 
 while True:
     command = input("\nChoose command:")
     if command == "1":
-        pass
+        expenses_write={}
+        expenses_write['name']=input("please write the expenses name: ")
+        expenses_write['sum']=int(input("please write the full sum: "))
+        expenses_write['category']=input("please write the expenses category: ")
+        expenses.append(expenses_write)
+        print(expenses)
+    if command == "2":
+        print(expenses)
+    if command == "3":
+        expenses_comp.sort(key = sort_price, reverse = True)
+        print(expenses_comp[0:10])
+    if command == "4":
+        expenses.sort(key = sort_price)
+        print(expenses[0:10])
+    if command == "5":
+       print(expenses)
     if command == "e":
+        with open ("expenses.json", "w") as outfiles:
+            json.dump(expenses, outfiles)
         print("Exiting...")
         break
 
